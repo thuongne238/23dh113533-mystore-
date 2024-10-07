4f5149d2c295bb00f975593078f9aef0c34b5067
thuongne238 <beattime2308@gmail.com>
2024-10-07 12:01:17 +0700

    Add project files.

---
+++ 23dh113533-mystore/Areas/admin/Controllers/homeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23dh113533_mystore.Areas.admin.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: admin/home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 23dh113533-mystore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23dh113533_mystore.MvcApplication" Language="C#" %>
